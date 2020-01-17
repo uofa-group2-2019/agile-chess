29e4b80729a1946377c3d7a5f8811e480e9f83d7
Liz Gehret <elizabeth.gehret@gmail.com>
2020-01-17 04:04:11 +0000

    Added STI for all of the pieces, and the pieces model/migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :games
+  has_many :pieces
   validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_28_001506) do
+ActiveRecord::Schema.define(version: 2020_01_17_022614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pieces", force: :cascade do |t|
+    t.string "name"
+    t.integer "x_coord"
+    t.integer "y_coord"
+    t.integer "game_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
